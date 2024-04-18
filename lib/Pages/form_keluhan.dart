0e0275e52efc740799c0b600fb92877c28a6c0d5
Legi <legikuswandi@gmail.com>
2024-04-18 20:24:56 +0700

    FK-belumberes

--- lib/Pages/form_keluhan.dart
+++ lib/Pages/form_keluhan.dart
@@ -12,17 +12,6 @@ class FormKeluhan extends StatefulWidget {
   State<FormKeluhan> createState() => _FormKeluhanState();
 }
 
-//Bikin di sini dulu ajalah wkwkwkwk
-List<String> gejalaList = [
-  "#Mual",
-  "#ISPA",
-  "#SakitGigi",
-  "#Muntah",
-  "#SakitKepala",
-  "#Pendarahan",
-  //Contoh hashtags
-];
-
 class _FormKeluhanState extends State<FormKeluhan> {
   @override
   Widget build(BuildContext context) {
@@ -45,7 +34,7 @@ class FormKeluhanContent extends StatefulWidget {
 class _FormKeluhanContentState extends State<FormKeluhanContent> {
   bool useBPJS = false;
   TextEditingController keluhanController = TextEditingController();
-
+  String selectedPatient = 'Legi Kuswandi';
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -55,19 +44,58 @@ class _FormKeluhanContentState extends State<FormKeluhanContent> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Text(
-            "Hai, Hafiizh",
+            "Buat Janji",
             style: getDefaultTextStyle(font_size: 25),
           ),
           Padding(
             padding: EdgeInsets.only(bottom: 20), // Menambahkan padding di bagian bawah
             child: Text(
-              "Selamat datang kembali di aplikasi SIRAJA!",
+              "Silahkan isi form untuk membuat janji",
               style: getDefaultTextStyle(),
             ),
           ),
-          Text(
-            "Buat reservasi rawat jalan!",
-            style: getDefaultTextStyle(font_size: 20),
+          SizedBox(height: 20), // Menambahkan jarak vertikal
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30), // Mengatur pinggiran menjadi rounded
+              color: Color(0xFF54d4da), // Mengatur warna latar belakang box
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10), // Memberikan padding di dalam box
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Siapa yang sakit?",
+                  style: getDefaultTextStyle(font_color: Colors.white, font_size: 18),
+                ),
+                DropdownButton<String>(
+                  // Buatlah list dari pasien yang dapat dipilih
+                  items: <String>['Legi Kuswandi', 'Franklin Turnip', 'Rahman Ismail']
+                      .map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value.length <= 8 ? value : value.substring(0, 8) + '..', // Batasi panjang teks menjadi 12 karakter
+                        overflow: TextOverflow.ellipsis, // Tambahkan elipsis (...) jika teks melebihi batas
+                        maxLines: 1,
+                        style: getDefaultTextStyle(font_color: Colors.white, font_size: 18), // Hanya satu baris yang ditampilkan
+                      ),
+                    );
+                  }).toList(),
+                  // Callback ketika nilai dropdown berubah
+                  onChanged: (String? newValue) {
+                    if (newValue != null) {
+                      setState(() {
+                        selectedPatient = newValue;
+                      });
+                    }
+                  },
+                  // Nilai awal dropdown
+                  value: selectedPatient, // Nilai default
+                  dropdownColor: Color(0xFF54d4da),
+                ),
+              ],
+            ),
           ),
           SizedBox(height: 20), // Menambahkan jarak vertikal
           Container(
@@ -87,7 +115,7 @@ class _FormKeluhanContentState extends State<FormKeluhanContent> {
                   children: [
                     Text(
                       useBPJS ? 'Ya' : 'Tidak', // Menampilkan "Ya" jika toggle aktif, dan "Tidak" jika tidak aktif
-                      style: getDefaultTextStyle(font_size: 15, font_color: Colors.white),
+                      style: getDefaultTextStyle(font_size: 18, font_color: Colors.white),
                     ),
                     Switch(
                       value: useBPJS,
@@ -109,7 +137,7 @@ class _FormKeluhanContentState extends State<FormKeluhanContent> {
           TextFormField(
             controller: keluhanController,
             decoration: InputDecoration(
-              hintText: 'Masukkan keluhan...', // Mengatur teks yang akan hilang saat masukan dimulai
+              hintText: 'Masukkan gejala...',
               hintStyle: getDefaultTextStyle(font_size: 18, font_color: const Color.fromARGB(255, 155, 155, 155)),
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(20), // Atur radius sesuai kebutuhan Anda
@@ -117,61 +145,6 @@ class _FormKeluhanContentState extends State<FormKeluhanContent> {
             ),
             style: getDefaultTextStyle(font_size: 18),
             maxLines: 5,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0), // Tambahkan padding bawah ke teks "Gejala"
-            child: Row(
-              children: [
-                Text(
-                  "Gejala",
-                  style: getDefaultTextStyle(font_size: 18, font_color: blackColor),
-                ),
-                SizedBox(width: 10), // Tambahkan jarak horizontal antara teks dan ikon
-                Icon(
-                  Icons.search, // Gunakan ikon kaca pencarian
-                  color: blackColor, // Sesuaikan warna ikon
-                  size: 24, // Sesuaikan ukuran ikon
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 10), // Menambahkan jarak vertikal
-          Padding(
-            padding: EdgeInsets.only(bottom: 20), // Tambahkan padding di bagian bawah tombol
-            child: Wrap(
-              alignment: WrapAlignment.start, // Meratakan widget ke kiri
-              children: List.generate(
-                gejalaList.length, // Menggunakan panjang list gejala sebagai batas iterasi
-                (index) => Padding(
-                  padding: EdgeInsets.only(right: 5.0, bottom: 5.0), // Atur padding kanan dan bawah
-                  child: HashtagGejala(
-                    label: gejalaList[index], // Menggunakan label dari gejalaList
-                    onPressed: () {},
-                  ),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 10),
-          ElevatedButton(
-            onPressed: () {
-              // Tambahkan logika untuk menangani reservasi di sini
-              // Contoh: Panggil fungsi atau tampilkan dialog konfirmasi
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xFF54d4da), // Gunakan warna primer dari tema aplikasi
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15), // Atur padding tombol
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)), // Bentuk pinggiran tombol
-            ),
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.8, // Lebar tombol 80% dari lebar layar
-              child: Center(
-                child: Text(
-                  'Reservasi',
-                  style: TextStyle(fontSize: 18, color: Colors.white), // Gaya teks tombol
-                ),
-              ),
-            ),
           ),
           SizedBox(height: 30),
           ElevatedButton(
@@ -181,19 +154,19 @@ class _FormKeluhanContentState extends State<FormKeluhanContent> {
             },
             style: ElevatedButton.styleFrom(
               backgroundColor: Color(0xFF54d4da), // Gunakan warna primer dari tema aplikasi
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15), // Atur padding tombol
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)), // Bentuk pinggiran tombol
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20), // Atur padding tombol
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)), // Bentuk pinggiran tombol
             ),
             child: Container(
               width: MediaQuery.of(context).size.width * 0.8, // Lebar tombol 80% dari lebar layar
               child: Center(
                 child: Text(
-                  'Rekomendasi Dokter',
-                  style: TextStyle(fontSize: 18, color: Colors.white), // Gaya teks tombol
+                  'Reservasi',
+                  style: getDefaultTextStyle(font_color: Colors.white, font_size: 18), // Gaya teks tombol
                 ),
               ),
             ),
-          )
+          ),
         ],
       ),
     );
