6205512410e678b20602b94ac615a296aebfed18
FITurnip <franklinirtr@gmail.com>
2024-06-07 18:35:15 +0700

    simplify store update pasien

--- lib/Pages/Profile/store_update_keluarga.dart
+++ lib/Pages/Profile/store_update_keluarga.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tubes/Model/pasien.dart';
-import 'package:tubes/Widget/store_update_keluarga.dart';
-import 'package:tubes/theme.dart';
+import 'package:tubes/Widget/store_update_pasien.dart';
 
 class StoreUpdateKeluargaPage extends StatelessWidget {
   final Pasien ?pasien;
@@ -10,56 +9,13 @@ class StoreUpdateKeluargaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: () {Navigator.of(context).pop();},),
+        elevation: 0,
+        forceMaterialTransparency: true,
+      ),
       body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        IconButton(
-                          icon: Icon(Icons.arrow_back),
-                          onPressed: () {
-                            // Navigate back when the button is pressed
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                        SizedBox(
-                          width: 60, // Atur lebar gambar
-                          height: 58, // Atur tinggi gambar
-                          child: Image.asset("assets/img/Logo.png"),
-                        ),
-                      ],
-                    ),
-                    Center(
-                      child: Text(
-                        "keluarga",
-                        style: getDefaultTextStyle(
-                            font_size: 24, font_weight: FontWeight.w600),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(30, 20, 30, 0),
-                child: Column(
-                  children: <Widget>[
-                    StoreUpdateKeluarga(pasien: pasien),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+        child: StoreUpdatePasien(pasien: pasien, title: "Anggota Keluarga", url: "profile/storeUpdate",),
       ),
     );
   }
