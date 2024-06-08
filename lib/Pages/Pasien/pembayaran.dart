f367699abe9e95fa5ab9a2f9225bbaf8c47aa6a9
Alfen <alfenfajri@gmail.com>
2024-06-08 11:57:29 +0700

    grid pembayaran

--- lib/Pages/Pasien/pembayaran.dart
+++ lib/Pages/Pasien/pembayaran.dart
@@ -1,7 +1,25 @@
+import 'dart:js';
+
 import 'package:tubes/Pages/Pasien/template.dart';
 import 'package:tubes/Widget/expansible_list.dart';
 import 'package:flutter/material.dart';
 import 'package:tubes/theme.dart';
+
+class PaymentOption {
+  final String name;
+  final String image;
+
+  PaymentOption({required this.name, required this.image});
+}
+
+List<PaymentOption> paymentOptions = [
+  PaymentOption(name: 'Dana', image: 'assets/img/pembayaran/DANA.png'),
+  PaymentOption(name: 'OVO', image: 'assets/img/pembayaran/Ovo.png'),
+  PaymentOption(name: 'Link Aja', image: 'assets/img/pembayaran/LinkAja.png'),
+  PaymentOption(
+      name: 'ShopeePay', image: 'assets/img/pembayaran/Shopee_pay.png'),
+  PaymentOption(name: 'Gopay', image: 'assets/img/pembayaran/Gopay.png'),
+];
 
 class Pembayaran extends PasienTemplate {
   Pembayaran()
@@ -10,7 +28,7 @@ class Pembayaran extends PasienTemplate {
     pasienTemplateItems = [
       ExpansibleItem(
           icon: Icon(Icons.person),
-          headerValue: "Pemeriksaan",
+          headerValue: "Invoice",
           expandedValue: Container(
             child: GestureDetector(
               onTap: () {
@@ -48,79 +66,40 @@ class Pembayaran extends PasienTemplate {
           ),
           isExpanded: true),
       ExpansibleItem(
-        icon: Icon(Icons.assignment),
-        headerValue: "Resep Obat",
-        expandedValue: GestureDetector(
-          onTap: () {
-            // buat upload gambar
-          },
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: blackColor,
-                  width: 5,
-                ),
-              ),
-              alignment: Alignment.center,
-              height: 200,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.upload_file,
-                    size: 40,
-                    color: blackColor,
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    "Unggah Bukti Pembayaran",
-                    style: getDefaultTextStyle(font_color: blackColor),
-                  ),
-                ],
-              ),
+          icon: Icon(Icons.assignment),
+          headerValue: "Pembayaran, Total: Rp.500.000.,",
+          expandedValue: GridView.builder(
+            padding: EdgeInsets.all(8.0),
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4, // Number of items per row
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
             ),
-          ),
-        ),
-      ),
-      ExpansibleItem(
-          icon: Icon(Icons.summarize),
-          headerValue: "Penunjang Medis 1",
-          expandedValue: Container(
-            child: GestureDetector(
-              onTap: () {
-                // buat upload gambar
-              },
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: blackColor,
-                      width: 5,
+            itemCount: paymentOptions.length,
+            itemBuilder: (context, index) {
+              final paymentOption = paymentOptions[index];
+              return GestureDetector(
+                onTap: () {
+                  print('${paymentOption.name} diklik');
+                },
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage(paymentOption.image),
                     ),
-                  ),
-                  alignment: Alignment.center,
-                  height: 200,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.upload_file,
-                        size: 40,
-                        color: blackColor,
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        "Unggah Bukti Pembayaran",
-                        style: getDefaultTextStyle(font_color: blackColor),
-                      ),
-                    ],
-                  ),
+                    SizedBox(height: 8),
+                    Text(
+                      paymentOption.name,
+                      style: getDefaultTextStyle(font_size: 12),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
                 ),
-              ),
-            ),
+              );
+            },
           )),
     ];
   }
